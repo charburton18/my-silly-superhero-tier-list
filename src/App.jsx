292158041a8f34e3984c5806f09159de52340aa7
Char B <charburton18@gmail.com>
2024-10-17 15:07:39 -0700

    App.jsx html finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Silly Superhero Tier List</h1>
+      <h2>Welcome!</h2>
+      <p>Welcome to MSSTL, where you can make super silly tier lists with your favorite superheroes!</p><br></br>
+      <p>Make a tier list of:</p>
+      <h2 id='tier-list-prompt'>The most overrated supers</h2>
+      <button>Reroll</button>
+      <button>Make my Tier List!</button>
     </>
   )
 }
