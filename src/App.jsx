fecf990196b1ef03e20e9373827d27283a133cde
Char B <charburton18@gmail.com>
2024-10-17 16:23:01 -0700

    home page functional except Make Tier List button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import './App.css'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <>
-      <h1>My Silly Superhero Tier List</h1>
-      <h2>Welcome!</h2>
-      <p>Welcome to MSSTL, where you can make super silly tier lists with your favorite superheroes!</p><br></br>
-      <p>Make a tier list of:</p>
-      <h2 id='tier-list-prompt'>The most overrated supers</h2>
-      <button>Reroll</button>
-      <button>Make my Tier List!</button>
+      <Home></Home>
     </>
   )
 }
