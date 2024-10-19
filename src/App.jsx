948e658f81a5c26025d3ca2abd8b9cab04ca498d
Char B <charburton18@gmail.com>
2024-10-19 14:54:44 -0700

    prompt prop is functioning

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Home from './Pages/Home'
 import Search from './Pages/Search'
 import Customizer from './Pages/Customizer'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+
+  const [prompt, setPrompt] = useState("Which superheroes are the most overrated?");
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} /> {/*prompt={prompt} setPrompt={setPrompt}*/}
-        <Route path='/search' element={<Search />} />
-        <Route path='/customizer' element={<Customizer />} />
+        <Route path='/' element={<Home prompt={prompt} setPrompt={setPrompt}/>} />
+        <Route path='/search' element={<Search prompt={prompt} setPrompt={setPrompt}/>} />
+        <Route path='/customizer' element={<Customizer prompt={prompt} setPrompt={setPrompt}/>} />
       </Routes>
     </>
   )
