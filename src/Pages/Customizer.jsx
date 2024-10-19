8631072a1cf7292b28483404b6c3a7fe8dac4d92
Char B <charburton18@gmail.com>
2024-10-19 13:54:13 -0700

    routing complete

---
+++ src/Pages/Customizer.jsx
@@ -0,0 +1,41 @@
+import NavBar from '../components/NavBar'
+import { Link } from 'react-router-dom'
+
+function Customizer() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <h3>My Silly Superhero Tier List of:</h3>
+      <h2 id='tier-list-prompt'>Which superheroes are the most overrated?</h2> {/* Need to pass prompt prop here */}
+
+      <ol>S Tier
+        <li></li>
+      </ol>
+      <ol>A Tier
+        <li></li>
+      </ol>
+      <ol>B Tier
+        <li></li>
+      </ol>
+      <ol>C Tier
+        <li></li>
+      </ol>
+      <ol>D Tier
+        <li></li>
+      </ol>
+      <ol>E Tier
+        <li></li>
+      </ol>
+      <ol>F Tier
+        <li></li>
+      </ol>
+
+      <Link to="/search">
+      <button>Edit my List</button>
+      </Link>
+    </>
+  )
+}
+
+export default Customizer
