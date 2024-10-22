2e1972f8014e04a60f5ce84b4a03b3f069cf0984
Char B <charburton18@gmail.com>
2024-10-22 13:12:47 -0700

    first pass on code cleanliness and formatting

--- src/Pages/Customizer.jsx
+++ src/Pages/Customizer.jsx
@@ -1,7 +1,7 @@
 import NavBar from '../components/NavBar'
 import { Link } from 'react-router-dom'
 
-function Customizer({ prompt, setPrompt, myTierList, setMyTierList }) {
+function Customizer({ prompt, myTierList, setMyTierList }) {
 
   return (
     <>
@@ -10,8 +10,7 @@ function Customizer({ prompt, setPrompt, myTierList, setMyTierList }) {
         <h3>My Silly Superhero Tier List of:</h3>
         <h2 id='tier-list-prompt'>{prompt}</h2> 
 
-        {console.log(myTierList)}
-
+        {/* maps through myTierList and returns the name of each character in myTierList */}
         <div id="list-div">
           <ol>
             {myTierList.map((currentChar) => {
@@ -25,12 +24,12 @@ function Customizer({ prompt, setPrompt, myTierList, setMyTierList }) {
           </ol>
         </div>
 
+        {/* the "Clear my List" button clears the myTierList state variable, setting it to an empty array */}
         <button className="bn633-hover bn18" onClick={() => setMyTierList([])}>Clear my List</button>
 
         <Link to="/search">
           <button className="bn632-hover bn18">Edit my List</button>
         </Link>
-
 
       </div>
     </>
